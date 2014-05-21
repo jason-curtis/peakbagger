514e009900f6ee969758e8aa9d8a239ea72a8391
waterproof <thatneat@gmail.com>
2014-05-21 00:41:17 -0400

    adding coffeescript and translating peakbagger.js

--- app/peakbagger.js
+++ app/peakbagger.js
@@ -1,19 +1,13 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to peakbagger.";
-  };
+if (Meteor.isClient)
+  Template.hello.greeting = -> "Welcome to peakbagger."
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+  Template.hello.events(
+    'click input' : ->
+      if console?
+        console.log("You pressed the button")
+  )
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+if (Meteor.isServer)
+  "code to run on server at startup"
+  Meteor.startup( -> null
+  )
